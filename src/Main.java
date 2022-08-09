54b832ef9666215f59ac638fb00232d98d85cb90
Hazal Nur Akyuz <hazalnurakyuz@outlook.com>
2022-08-09 18:24:46 +0300

    a

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int r;
+        double alan, hacim, pi = 3.14;
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Dairenin Yaricapini Giriniz:");
+        r = input.nextInt();
+        alan = 2 * pi * r;
+        hacim = pi * (r*r);
+        System.out.println("Dairenin Alani:" + alan);
+        System.out.println("Dairenin Hacmi:" + hacim);
+
     }
 }
